a5483d59fd49b911a448488e84a84ef800ddc849
Erik Lightner <erikdlightner@gmail.com>
2022-03-30 20:21:21 -0500

    Initial Commit

---
+++ Random Generator/Random_GeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Random_GeneratorApp.swift
+//  Random Generator
+//
+//  Created by Erik Lightner on 3/30/22.
+//
+
+import SwiftUI
+
+@main
+struct Random_GeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
